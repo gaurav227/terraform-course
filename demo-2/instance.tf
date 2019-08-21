256b0064f789dd2d29318054bd890d63944e5fd8
gaurav227 <gaurav.narasimhan@gmail.com>
2019-08-20 23:20:14 -0700

    testing terraform

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "mykey" {
 
 resource "aws_instance" "example" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
+  instance_type = "a1.medium"
   key_name = "${aws_key_pair.mykey.key_name}"
 
   provisioner "file" {
--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = "AKIARTUOVM5ENNIOXFGD"
+  secret_key = "YBlBiwLAL6fpkiS3C3SqxntlsCOvgJYQgnY+fWaQ"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
-  instance_type = "t2.micro"
+  ami           = "ami-0f01fcbe971af8f5a"
+  instance_type = "a1.medium"
 }
